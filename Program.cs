2964cf213e6592c815703fc71bceae6785cafac4
Jorge Blanchard Cruz <rothgard@live.com>
2025-07-31 19:47:28 +0100

    Add license file and license header to source code
    Added LICENSE.txt with Creative Commons Attribution 4.0 (CC BY 4.0) terms. Also included a license header in Program.cs to clarify licensing and donation information.

--- Program.cs
+++ Program.cs
@@ -77,3 +77,12 @@ public class Program
     }
 
 }
+
+#region License
+
+// Copyright (c) 2025 Jorge Blanchard Cruz
+// Licensed under the Creative Commons Attribution 4.0 License (CC BY 4.0).
+// For more information, visit: https://creativecommons.org/licenses/by/4.0/
+// I accept donations at: contact me at jorgeblanchardcruz@outlook.com
+
+#endregion License
